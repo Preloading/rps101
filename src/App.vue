162e9dc39689d7f9af1f9be05d5239639ea53c2b
Logan <60761520+Preloading@users.noreply.github.com>
2023-10-12 17:55:50 -0600

    It's a start

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
