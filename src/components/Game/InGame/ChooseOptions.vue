a351688792628c07fccfb87251a66825c54c5c49
Preloading <loganhus4005@gmail.com>
2023-11-21 14:43:20 -0700

    Add buttons to chooseoptions

--- src/components/Game/InGame/ChooseOptions.vue
+++ src/components/Game/InGame/ChooseOptions.vue
@@ -7,17 +7,21 @@
         </div>
         <h2>VS</h2>
         <div class="rounded waitingPlayer">
-            <img :alt="username + '\'s avatar'" :src="avatar" class="col rounded"> 
+            <img :alt="username + '\'s avatar'" :src="avatar" class="col rounded" > 
             <span class="usernameHost">{{ username }}</span>
         </div>
-        <ul>
-            <li></li>
+        <ul class="d-flex flex-wrap">
+            <button v-for='outcome in outcomes' class="list-unstyled text-center btn">
+                    <img :src="'/outcomes/' + outcome.img" class="m-1" style="height: 64px; width: 64px;">
+                    <p class="text-center">{{ outcome.title.charAt(0).toUpperCase() + outcome.title.slice(1) }}</p>
+                
+
+            </button>
         </ul>
     </div>
 </template>
 <script setup>
 import outcomes from "../../../assets/outcomes/data.json"
-console.log(outcomes[player1result].compares);
 function getWinner(player1result, player2result) {
     if (outcomes[player1result].compares.filter(e => e.other_gesture_id === player2result)) {
         return 1;
@@ -29,4 +33,5 @@ function getWinner(player1result, player2result) {
         }
     }
 }
+
 </script>
--- src/components/Game/InGame/PlayerInGame.vue
+++ src/components/Game/InGame/PlayerInGame.vue
@@ -1,6 +1,7 @@
 <template>
     GAMEPLAY HAS STARTED WOOP WOOP!!!
+    <ChooseOptions />
 </template>
 <script setup>
-
+import ChooseOptions from '../InGame/ChooseOptions.vue'
 </script>
