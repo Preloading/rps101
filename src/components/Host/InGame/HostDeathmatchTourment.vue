eab2c912654cf509fb3ef94db8bf242a56241f58
Logan <60761520+Preloading@users.noreply.github.com>
2023-11-28 20:37:09 -0700

    WHAT IS WRONG WITH DEFINEPROPS?!?!?

--- src/components/Host/InGame/HostDeathmatchTourment.vue
+++ src/components/Host/InGame/HostDeathmatchTourment.vue
@@ -11,21 +11,21 @@ import { collection, doc, orderBy, query,updateDoc } from 'firebase/firestore'
 import { gamesRef } from '../../../firebase.js'
 console.log(-3)
 
-const props = defineProps(["game-doc-id"]);
-//const emit = defineEmits(["host-state"])
-console.log(-2)
+const props = defineProps("game-doc-id");
+// //const emit = defineEmits(["host-state"])
+// console.log(-2)
 
-console.log(props.gameDocId)
-const gameRef = doc(gamesRef, props.gameDocId)
-const playersRef = collection(gameRef, "players")
-const players = useCollection(query(playersRef, orderBy("timestamp")))
-alert(1);
-// Create the matches
-const matchedPlayers = players.data.value.sort(() => Math.random() - 0.5); // Sort players
-if (matchedPlayers.length % 2 == 0) {
-   // TODO: add Evilbot
-}
-alert(matchedPlayers[1].id)
+// console.log(props.gameDocId)
+// const gameRef = doc(gamesRef, props.gameDocId)
+// const playersRef = collection(gameRef, "players")
+// const players = useCollection(query(playersRef, orderBy("timestamp")))
+// alert(1);
+// // Create the matches
+// const matchedPlayers = players.data.value.sort(() => Math.random() - 0.5); // Sort players
+// if (matchedPlayers.length % 2 == 0) {
+//    // TODO: add Evilbot
+// }
+// alert(matchedPlayers[1].id)
  
 
 </script>
--- src/components/Host/InGame/HostInGame.vue
+++ src/components/Host/InGame/HostInGame.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <h1>Host in game</h1>
-        <HostDeathmatchTourment :game-doc-id="props.game-doc-id"/>
+        <HostDeathmatchTourment :game-doc-id="props.gameDocId"/>
     </div>
 </template>
 <script setup>
 import HostDeathmatchTourment from './HostDeathmatchTourment.vue';
-const props = ref(defineProps(["game-doc-id"]));
+const props = defineProps(["game-doc-id"]);
 
 </script>
