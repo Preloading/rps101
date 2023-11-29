b1b02e87688d8ae8af4b510b8f6d59a1ea21c6a2
Logan <60761520+Preloading@users.noreply.github.com>
2023-11-29 07:51:25 -0700

    i am stupid

--- src/components/Host/InGame/HostDeathmatchTourment.vue
+++ src/components/Host/InGame/HostDeathmatchTourment.vue
@@ -4,7 +4,7 @@
    </div>
 </template>
 
-<script>
+<script setup>
 //sort(() => Math.random() - 0.5)
 import { useCollection } from 'vuefire'
 import { collection, doc, orderBy, query,updateDoc } from 'firebase/firestore'
@@ -12,20 +12,20 @@ import { gamesRef } from '../../../firebase.js'
 console.log(-3)
 
 const props = defineProps("game-doc-id");
-// //const emit = defineEmits(["host-state"])
-// console.log(-2)
+//const emit = defineEmits(["host-state"])
+console.log(-2)
 
-// console.log(props.gameDocId)
-// const gameRef = doc(gamesRef, props.gameDocId)
-// const playersRef = collection(gameRef, "players")
-// const players = useCollection(query(playersRef, orderBy("timestamp")))
-// alert(1);
-// // Create the matches
-// const matchedPlayers = players.data.value.sort(() => Math.random() - 0.5); // Sort players
-// if (matchedPlayers.length % 2 == 0) {
-//    // TODO: add Evilbot
-// }
-// alert(matchedPlayers[1].id)
+console.log(props.gameDocId)
+const gameRef = doc(gamesRef, props.gameDocId)
+const playersRef = collection(gameRef, "players")
+const players = useCollection(query(playersRef, orderBy("timestamp")))
+alert(1);
+// Create the matches
+const matchedPlayers = players.data.value.sort(() => Math.random() - 0.5); // Sort players
+if (matchedPlayers.length % 2 == 0) {
+   // TODO: add Evilbot
+}
+alert(matchedPlayers[1].id)
  
 
 </script>
